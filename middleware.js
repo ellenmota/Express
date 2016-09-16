f4c83673c765d6dc603c56b31e16227a4b379d08
Ellen Bianca Mota de Carvalho <ellen.carvalho@MIDGARD.LOCAL>
2016-09-16 16:06:55 -0300

    Tópico usando Middleware

--- middleware.js
+++ middleware.js
@@ -6,16 +6,51 @@ app.use(function (req,res,next){
   next();
 });
 
-app.use('/user/:id',function(req,res,next){
-  console.log('Request Type: ',req.method);
-  next();
-});
+// app.use('/user/:id',function(req,res,next){
+//   console.log('Request Type: ',req.method);
+//   next();
+// });
 
-app.get('/user/:id',function(req,res,next){
-  console.log(req);
+app.get('/user/:id',function (req,res,next){
+  // console.log(req);
   res.send('User');
 });
 
-app.listen(3000, function(){
+//Middleware com varias funções
+app.get('/user/:id',function (req,res,next){
+  console.log('Requisição: ', req.originalURL);
+  next();
+}, function(req,res,next){
+    console.log('Request: ');
+    next();
+});
+
+//Varios caminhos para a mesma rota
+app.get('/user/:id', function (req, res, next){
+  console.log('Requisição user: ', req.params.id);
+  next();
+}, function (req, res, next){
+  res.send('User info.');
+});
+
+app.get('/user/:id', function (req, res, next){
+  res.end(req.params.id);
+});
+
+//rotas condicionais com rotas
+app.get('/user/:id',function (req,res,next){
+  if(req.params.id == 0)
+    next('route');
+  else
+    next();
+}, function (req,res,next) {
+    res.render('regular');
+});
+
+app.get('user/:id', function (req, res, next){
+  res.render('special');
+});
+
+app.listen(3000, function (){
   console.log('Test Middleware...');
 });
