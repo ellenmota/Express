ad8f9cd2adcbe5b45bbf0bf48a957fafd5ed1b0d
Ellen Bianca Mota de Carvalho <ellen.carvalho@MIDGARD.LOCAL>
2016-09-20 18:41:40 -0300

    Manipulação de erro

--- middleware.js
+++ middleware.js
@@ -51,9 +51,29 @@ app.get('user/:id', function (req, res, next){
   res.render('special');
 });
 
+//Manipulação de Erro
+app.use(function(err,req,res,next){
+  console.error(err.stack);
+  res.status(500).send('Algo quebrou!')
+});
+
+
+//Mecanismo modelo, ou seja, html dinamico
+app.set('view engine','jade');
+
+app.get('/', function(req,res){
+  res.render('index',{title:'Hey',message: 'Hello!'});
+});
+
+//Manipulação de error, é usada no final de todos os use, e cria-se funções separadas
+function erros(err,req,res,next){
+  console.error(err.stack);
+  next(err);
+}
 
 
 
+//Porta
 app.listen(3000, function (){
   console.log('Test Middleware...');
 });
